810a1fc94911f5678723f221510a60568d3aaadc
Jakub Hava <jakub.hava@gmail.com>
2016-11-28 11:37:02 +0100

    Remove word core project names and create server project

--- examples/H2OSumMRTaskTest/agent/src/main/java/water/MRTaskInterceptor.java
+++ examples/H2OSumMRTaskTest/agent/src/main/java/water/MRTaskInterceptor.java
@@ -8,6 +8,7 @@ import net.bytebuddy.implementation.bind.annotation.This;
 public class MRTaskInterceptor implements Interceptor {
     public void compute2(@This Object o) {
         if (o instanceof SumMRTask) {
+            // start span
             MRTask task = (MRTask) o;
             System.out.println("Compute2 (Local work) was called on node: " + H2O.getIpPortString() + " trace ID " + InstrumentUtils.getTraceId(o));
         }
@@ -15,7 +16,7 @@ public class MRTaskInterceptor implements Interceptor {
 
     public void onCompletion(@This Object o) {
         if (o instanceof SumMRTask) {
-
+            // close span
             MRTask task = (MRTask) o;
             System.out.println("OnCompletition ( local reducing) was called on node: " + H2O.getIpPortString() + " trace ID " + InstrumentUtils.getTraceId(o));
         }
@@ -29,6 +30,7 @@ public class MRTaskInterceptor implements Interceptor {
     }
 
     public void doAll(@This Object o){
+        // start main span
         if( o instanceof SumMRTask) {
             InstrumentUtils.setTraceId(o);
             System.out.println("doAll was called on node: " + H2O.getIpPortString() + " trace ID " + InstrumentUtils.getTraceId(o));
