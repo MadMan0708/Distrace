b3cfe903b11ba935084f5108d0f6d35487410494
Jakub Hava <jakub.hava@gmail.com>
2017-01-03 20:35:40 +0100

    Tiny cleanup of MainWithTask

--- examples/H2OSumMRTaskTest/app/src/main/java/cz/cuni/mff/d3s/distrace/examples/MainWithTask.java
+++ examples/H2OSumMRTaskTest/app/src/main/java/cz/cuni/mff/d3s/distrace/examples/MainWithTask.java
@@ -18,30 +18,32 @@ public class MainWithTask {
         H2OApp.main(args);
         // Wait for rest of the cloud, for 10 seconds max
         H2O.waitForCloudSize(2, 10000);
-
         // Create frame with numbers we want to count
         Vec numVec = Vec.makeSeq(3, 10000000);
         Frame frame = new Frame(numVec);
-        System.out.println("Number of chunks on frame: " + frame.anyVec().nChunks());
+        printFrameInfo(frame);
+
+        // Start Sum MR task 3 times
+        for(int i = 0; i<3; i++){
+            SumMRTask mrTask = new SumMRTask().doAll(frame);
+            System.out.println("Computed sum is " + mrTask.getSum());
+        }
+        
+        // Shutdown the cluster once we have the result
+        H2O.shutdown(0);
+    }
+
+    private static void printFrameInfo(Frame fr){
+        System.out.println("Number of chunks on frame: " + fr.anyVec().nChunks());
         System.out.println("Frame distributed on nodes:");
         HashSet<H2ONode> uniqueNodes = new HashSet<>();
-        for(int i = 0; i<frame.anyVec().nChunks(); i++){
-            H2ONode node = frame.anyVec().chunkKey(i).home_node();
+        for(int i = 0; i<fr.anyVec().nChunks(); i++){
+            H2ONode node = fr.anyVec().chunkKey(i).home_node();
             if(!uniqueNodes.contains(node)){
                 System.out.println(node);
                 uniqueNodes.add(node);
             }
         }
         System.out.println();
-
-        // Start Sum MR task 3 times
-        for(int i = 0; i<3; i++){
-            SumMRTask mrTask = new SumMRTask().doAll(frame);
-            long sum = mrTask.getResult().getSum();
-            System.out.println("Computed sum is " + sum);
-        }
-        
-        // Shutdown the cluster once we have the result
-        H2O.shutdown(0);
     }
 }
